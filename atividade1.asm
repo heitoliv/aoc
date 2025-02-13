74a6e214127dd33286e5cdcc569329fd2d86f142
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2025-02-13 20:30:06 -0300

    Add files via upload

---
+++ atividade1.asm
@@ -0,0 +1,84 @@
+;Problema: Verificacao de Ano Bissexto 
+
+;Escreva um programa que receba um ano como entrada e determine se ele eh bissexto. Um ano eh bissexto se:
+;For divisivel por 4, mas nao por 100, ou
+;For divisivel por 400.
+;O programa deve exibir "Ano bissexto" ou "Ano nao bissexto" com base na verificacao
+
+ORG 100h    
+
+LEA DX, input
+MOV AH, 09h
+int 21h
+                
+MOV AH, 01h
+INT 21h 
+SUB AL, '0'  
+MOV BL, AL  
+MOV AL, BL
+MOV AH, 10
+MUL AH
+
+MOV AH, 01h
+INT 21h
+SUB AL, '0'
+MOV BH, AL   
+ADD AL, BH
+MOV AH, 10
+MUL AH
+
+MOV AH, 01h
+INT 21h
+SUB AL, '0'
+MOV CL, AL  
+ADD AL, CL
+MOV AH, 10
+MUL AH
+
+MOV AH, 01h
+INT 21h
+SUB AL, '0'
+MOV CH, AL  
+ADD AL, CH
+MOV CX, AX  
+
+MOV AX, CX
+MOV DX, 0
+MOV BX, 4
+DIV BX
+CMP DX, 0
+JNZ nao_bissexto
+
+MOV AX, CX
+MOV DX, 0
+MOV BX, 100
+DIV BX
+CMP DX, 0
+JNZ bissexto      
+
+MOV AX, CX
+MOV DX, 0
+MOV BX, 400
+DIV BX
+CMP DX, 0
+JZ bissexto  
+JMP nao_bissexto    
+    
+nao_bissexto:
+LEA DX, naobissexto
+MOV AH, 09h
+INT 21h
+JMP fim
+
+bissexto:
+LEA DX, ehbissexto
+MOV AH, 09h
+INT 21h
+
+fim:
+MOV AH, 4Ch
+INT 21h
+
+input           DB 13,10, 'Digite o ano: $'
+ehbissexto      DB 13,10, 'Ano bissexto$'
+naobissexto     DB 13,10, 'Ano nao bissexto$'    
